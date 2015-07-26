eee2171854e9d0fd147b9578b7d4065448dc0d3d
Irene Ma <iring.ma@gmail.com>
2015-07-26 16:00:39 -0700

    add working solution

--- hackerrank/arithmetic_progression.rb
+++ hackerrank/arithmetic_progression.rb
@@ -1,14 +1,14 @@
 def missing_num(elements_in_progression, prog_array)
-  p range = prog_array[-1] - prog_array[0]
-  p ap = range/elements_in_progression
+  range = prog_array[-1] - prog_array[0]
+  ap = range/elements_in_progression
   prog_array.each_with_index do |num, index|
     if index == (elements_in_progression-1)
-      return
+      next
     elsif (prog_array[index+1] - num) != ap
-      missing_num = num + ap
+      return missing_num = num + ap
     end
   end
-  p missing_num
 end
 
-p missing_num(5,[1,3,5,9,11])
+p missing_num(5,[1,3,5,9,11]) ==  7
+p missing_num(8,[1,5,13,17,21,25,29,33]) ==  9
